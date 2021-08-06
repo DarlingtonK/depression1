e50c84b9ba34575a5491b4bb2ab9ffb5957e10f4
DarlingtonK <84282906+DarlingtonK@users.noreply.github.com>
2021-08-06 22:56:07 +0200

    Update app.py

--- app.py
+++ app.py
@@ -131,7 +131,7 @@ def main():
   st.title("\tSocial Media Depression Detection")
   st.write(" ")
   st.write(" ")
-  st.sidebar.image('sentiments.jpg')
+  st.sidebar.image('th.jpg')
   st.sidebar.header("Project Partners")
   st.sidebar.text('1.Darlington Kwenda, R195903B')
   st.sidebar.text('2.Melissa Chikanyire, R197988X')
